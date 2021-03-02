3998437c4e7f04a3edcfd21a5451bea5aff3fbf0
polz <polz@fri.uni-lj.si>
2021-03-02 14:26:31 +0100

    dodani URL-ji za preverjanje podatkov za vnos v AD

--- php/apis-rilec-laravel/routes/api.php
+++ php/apis-rilec-laravel/routes/api.php
@@ -6,8 +6,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApisHRMasterController;
 use App\Http\Controllers\ApisOUController;
 use App\Http\Controllers\ApisUserProfileController;
-use App\Http\Controllers\ApisGroupAssignmentController;
-use App\Http\Controllers\LDAPActionController;
+use App\Http\Controllers\LdapActionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -30,19 +29,29 @@ Route::group(['middleware'=>'auth:api'], function(){
     Route::get('/api/ou/{date}', [ApisOUController::class, 'index']);
     Route::get('/api/ou/', [ApisOUController::class, 'index']);
     /* Podrobnosti org. enote v nekem trenutku */
-    Route::get('/api/ou/{date}/{id}', 'ApisOUController@show');
+    Route::get('/api/ou/{date}/{uid}', 'ApisOUController@show');
     /* Drevo org. enot v nekem trenutku */
     Route::get('/api/outree/{date}', 'ApisOUController@tree_index');
+    Route::get('/api/outree/', 'ApisOUController@tree_index');
     /* Drevo org. enot z uporabniki v nekem trenutku */
-    Route::get('/api/usertree/{date}', 'ApisUserProfileController@tree_index');
-    /* Skupine, v katerih je v nekem trenutku posameznik - morda spada v userprofile */
+    Route::get('/api/usertree/{date}', 'ApisOUController@user_tree_index');
+    Route::get('/api/usertree/', 'ApisOUController@user_tree_index');
+    /* Skupine, v katerih je v nekem trenutku posameznik - morda spada v ApisOUController */
     Route::get('/api/usermemberof/{date}/{id}', 'ApisUserProfileController@memberof');
     /* Pripadniki skupine v nekem trenutku - morda spada v podrobnosti enote */
-    Route::get('/api/groupmembers/{date}/{id}', 'ApisOUController@members');
-    /* Seznam kopiranj v AD */
-    Route::get('/api/ldapaction/', 'GroupAssignmentController@index');
+    Route::get('/api/oumembers/{date}/{uid}', 'ApisOUController@members');
+    /* Seznam razporeditev po OE */
+    Route::get('/api/groupassignment/{date}', 'GroupAssignmentController@index');
+    Route::get('/api/groupassignment/', 'GroupAssignmentController@index');
+    /* Podrobnosti prenosa podatkov v AD */
+    /* Seznam podatkov za kopiranje v AD. Vključuje UserData in GroupAssignments. */
+    Route::get('/api/dataset/', 'ADDatasetController@index');
+    Route::get('/api/dataset/{id}', 'ADDatasetController@show');
+    /* Ustvarjanje dataset-ov */
+    Route::put('/api/dataset/{timestamp}', 'ADDatasetController@create');
+    Route::put('/api/dataset/', 'ADDatasetController@create');
     /* Podrobnosti kopiranja v AD */
-    Route::get('/api/ldapaction/{id}', 'GroupAssignmentController@id');
+    Route::get('/api/ldapaction/{id}', 'LDAPActionController@id');
     /*
     Route::get('/user', function (Request $request) {
     return $request->user();
