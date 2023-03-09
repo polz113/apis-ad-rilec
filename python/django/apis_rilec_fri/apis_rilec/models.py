e9848ffee303db998c9f305cd04259af9a409290
Polz <polz@polz.si>
2023-03-09 07:37:35 +0100

    Napol prepisano - userdata, userdatafield

--- python/django/apis_rilec_fri/apis_rilec/models.py
+++ python/django/apis_rilec_fri/apis_rilec/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.utils.datastructures import MultiValueDict
 
 from collections import defaultdict
 
@@ -15,69 +16,101 @@ class DataSource(models.Model):
     data = models.BinaryField()
     def parsed_json(self):
         return json.loads(self.data)
+    
+    def _to_userdatadicts(self, timestamp, prefix, dataitem):
+        valid_from_d = dataitem['veljaOd']
+        valid_to_d = dataitem['veljaDo']
+        infotip = dataitem.get('infotip', '0000')
+        podtip = dataitem.get('podtip', '0')
+        datadicts = list()
+        for extraf in ['clanica_Id', 'kadrovskaSt']:
+            val = dataitem.get(extraf, None)
+            if val is None:
+                continue
+            d = { 'field': prefix + '.' + extraf,
+                  'value': val,
+                  'changed_t': timestamp,
+                  'valid_from': valid_from_d,
+                  'valid_to': valid_to_d }
+            datafields.append(UserDataField)
+        for subitem in dataitem.get('data', []):
+            subprefix = ".".join(prefix, infotip, podtip)
+            valid_from = subitem.get('veljaOd', valid_from_d)
+            valid_to = subitem.get('veljaDo', valid_to_d)
+            changed_t = subitem.get('datumSpremembe', timestamp)
+            for prop, val in subitem.items():
+                # Do not add timestamps or seq. number as properties
+                if prop in {'veljaOd', 'veljaDo', 'datumSpremembe', "stevilkaSekvence"}:
+                    continue
+                # Generate property name
+                d = { 'field': prefix + '.' + prop,
+                      'value': val,
+                      'changed_t': changed_t,
+                      'valid_from': valid_from_d,
+                      'valid_to': valid_to_d }
+                datadicts.append(d)
+        return datadicts
 
+    def _to_oudata(self, dataitem):
+        valid_from_d = dataitem.get('veljaOd')
+        valid_to_d = dataitem.get('veljaDo')
+        for subitem in dataitem['data']:
+            ou_name = subitem['organizacijskaEnota']
+            ou_shortname = subitem['organizacijskaEnota_kn']
+            valid_from = subitem.get('veljaOd', valid_from_d)
+            valid_to = subitem.get('veljaDo', valid_to_d)
+            changed_t = subitem.get('datumSpremembe', timestamp)
+            d = {'name': ou_name, 'shortname': ou_shortname}
+            ou_data_sets[(valid_from, valid_to, changed_t, clanica)][oe_id].update(d)
+        return ou_data_sets
+
+    def _to_ourelations(self, dataitem):
+        valid_from_d = dataitem.get('veljaOd')
+        valid_to_d = dataitem.get('veljaDo')
+        relation = dataitem.get('infotip', '0000') + '.' + \
+                   dataitem.get('podtip', '0000')
+        for subitem in dataitem['data']:
+            valid_from = subitem.get('veljaOd', valid_from_d)
+            valid_to = subitem.get('veljaDo', valid_to_d)
+            changed_t = subitem.get('datumSpremembe', timestamp)
+            ou_tup = (valid_from, valid_to, changed_t, clanica)
+            ou_parents[ou_tup][oe_id].append((relation, subitem['id']))
+        return ou_parents
+    
     def _to_datasets_apis(self):
         in_data = self.parsed_json()
         try:
             timestamp = timezone.datetime.fromisoformat(in_data['TimeStamp'])
         except KeyError:
             timestamp = self.timestamp
-        user_data_sets = defaultdict(dict)
         ou_data_sets = defaultdict(lambda: defaultdict(dict))
         ou_parents = defaultdict(lambda: defaultdict(list))
+        user_dicts = defaultdict(list)
         for k, v in in_data.items():
             if type(v) == list:
                 for dataitem in v:
-                    valid_from_d = dataitem.get('veljaOd')
-                    valid_to_d = dataitem.get('veljaDo')
-                    infotip = dataitem.get('infotip', '0000')
-                    podtip = dataitem.get('podtip', '0')
-                    # try to handle dataitem as user data
-                    try:
-                        ul_id = dataitem['UL_Id']
-                        clanica = dataitem['clanica_Id']
-                        kadrovska = dataitem['kadrovskaSt']
-                        for subitem in dataitem.get('data', []):
-                            d = dict()
-                            prefix = "{}.".format(k, infotip, podtip)
-                            for prop, val in subitem.items():
-                                # Do not add timestamps or seq. number as properties
-                                if prop in {'veljaOd', 'veljaDo', 'datumSpremembe', "stevilkaSekvence"}:
-                                    continue
-                                # Generate property name
-                                d[prefix + prop] = val
-                            valid_from = subitem.get('veljaOd', valid_from_d)
-                            valid_to = subitem.get('veljaDo', valid_to_d)
-                            changed_t = subitem.get('datumSpremembe', timestamp)
-                            user_data_sets[
-                                    (valid_from, valid_to, changed_t, clanica, kadrovska, ul_id)
-                                ].update(d)
-                    except KeyError:
-                        pass
-                    # try to handle dataitem as OU data
-                    try:
-                        oe_id = dataitem['OE_Id']
-                        clanica = dataitem['clanica_Id']
-                        if k == 'OE':
-                            for subitem in dataitem['data']:
-                                ou_name = subitem['organizacijskaEnota']
-                                ou_shortname = subitem['organizacijskaEnota_kn']
-                                valid_from = subitem.get('veljaOd', valid_from_d)
-                                valid_to = subitem.get('veljaDo', valid_to_d)
-                                changed_t = subitem.get('datumSpremembe', timestamp)
-                                d = {'name': ou_name, 'shortname': ou_shortname}
-                                ou_data_sets[(valid_from, valid_to, changed_t, clanica)][oe_id].update(d)
-                        elif k == 'NadrejenaOE':
-                            relation = dataitem.get('infotip', '0000') + '.' + \
-                                       dataitem.get('podtip', '0000')
-                            for subitem in dataitem['data']:
-                                valid_from = subitem.get('veljaOd', valid_from_d)
-                                valid_to = subitem.get('veljaDo', valid_to_d)
-                                changed_t = subitem.get('datumSpremembe', timestamp)
-                                ou_tup = (valid_from, valid_to, changed_t, clanica)
-                                ou_parents[ou_tup][oe_id].append((relation, subitem['id']))
-                    except KeyError:
-                        pass
+                    if k == 'OE':
+                        # TODO: fix this
+                        ou_data = self._to_oudata(dataitem)
+                    elif k == 'NadrejenaOE':
+                        # TODO: fix this
+                        ou_parents = self._to_ourelations(dataitem)
+                    else:
+                        uid = dataitem.get('UL_Id', None)
+                        if uid is None:
+                            continue
+                        else:
+                            user_dicts[uid] += self._to_userfields(timestamp, k, dataitem)
+        user_fields = list()
+        for uid, fieldlist in user_dicts.items():
+            ud = UserData(timestamp=timestamp, source=self, uid=uid)
+            ud.save()
+            for fields in fieldlist:
+                uf = UserDataField(userdata=ud, **fields)
+                user_fields.append(uf)
+        if len(user_fields):
+            UserDataField.objects.bulk_create(user_fields)
+        # TODO: remove the rest of this function
         # create user datasets
         for k, v in user_data_sets.items():
             valid_from, valid_to, changed_t, clanica, kadrovska, ul_id = k
@@ -155,7 +188,11 @@ class DataSet(models.Model):
 class OUData(models.Model):
     def __str__(self):
         return("{}: {} ({})".format(self.shortname, self.name, self.dataset))
-    dataset = models.ForeignKey('DataSet', on_delete=models.CASCADE)
+    # dataset = models.ForeignKey('DataSet', on_delete=models.CASCADE)
+    timestamp = models.DateTimeField()
+    valid_from = models.DateTimeField()
+    valid_to = models.DateTimeField()
+    source = models.ForeignKey('DataSource', on_delete=models.CASCADE)
     uid = models.CharField(max_length=64)
     name = models.CharField(max_length=256)
     shortname = models.CharField(max_length=32)
@@ -163,22 +200,35 @@ class OUData(models.Model):
 class OURelation(models.Model):
     def __str__(self):
         return("{}: {}-{} ({})".format(self.relation, self.ou1_id, self.ou2_id, self.dataset))
-    dataset = models.ForeignKey('DataSet', on_delete=models.CASCADE)
+    timestamp = models.DateTimeField()
+    source = models.ForeignKey('DataSource', on_delete=models.CASCADE)
+    valid_from = models.DateTimeField()
+    valid_to = models.DateTimeField()
+    # dataset = models.ForeignKey('DataSet', on_delete=models.CASCADE)
     relation = models.CharField(max_length=64)
     ou1_id = models.CharField(max_length=32)
     ou2_id = models.CharField(max_length=32)
 
 class UserData(models.Model):
     def __str__(self):
-        return("{}: {} ({})".format(self.field, self.data, self.dataset))
-    dataset = models.ForeignKey('DataSet', on_delete=models.CASCADE)
-    field = models.CharField(max_length=256)
-    data = models.CharField(max_length=512)
+        return("{} ({})".format(self.uid, self.source.id))
+    timestamp = models.DateTimeField()
+    source = models.ForeignKey('DataSource', on_delete=models.CASCADE)
+    uid = models.CharField(max_length=64)
+    # dataset = models.ForeignKey
+    # field = models.CharField(max_length=256)
+    # data = models.CharField(max_length=512)
 
-class LDAPActionBatch(models.Model):
-    description = models.CharField(max_length=512, blank=True, default='')
-    datasets = models.ManyToManyField('DataSet')
-    actions = models.ManyToManyField('LDAPAction')
+class UserDataField(models.Model):
+    def __str__(self):
+        return("{}: {} ({})".format(self.userdata, self.field, self.data))
+    userdata = models.ForeignKey('UserData', on_delete=models.CASCADE, related_name='fields')
+    valid_from = models.DateTimeField(null=True)
+    valid_to = models.DateTimeField(null=True)
+    changed_t = models.DateTimeField(null=True)
+    field = models.CharField(max_length=256)
+    value = models.CharField(max_length=512)
+    
 
 def _datasets_at(timestamp=None, source=None):
     if timestamp is None:
@@ -191,54 +241,109 @@ def _datasets_at(timestamp=None, source=None):
     return dsets.order_by('timestamp')
 
 
-def userdata_from_datasets(timestamp=None, source=None):
+def userdata_from_datasets(datasets):
     users = defaultdict(dict)
-    for ds in _datasets_at(timestamp, source):
+    sources = dict()
+    for ds in datasets.prefetch_related('userdata_set'):
         d = {}
         for ud in ds.userdata_set.all():
             d[ud.field] = ud.data
         ulid = d.pop('UL_Id', None)
         if ulid is None:
             continue
+        for k in d.keys():
+            sources[(ulid, k)] = ds.id
         users[ulid].update(d)
-    return users
+    return users, set(sources.values())
 
-def outrees_from_datasets(timestamp=None, source=None):
+def userdata_at(timestamp=None, source=None):
+    datasets = _datasets_at(timestamp, source)
+    return userdata_from_datasets(datasets)
+
+def outrees_from_datasets(datasets):
     ous = dict()
     id_relations = defaultdict(dict)
-    for ds in _datasets_at(timestamp, source):
+    oud_sources = dict()
+    our_sources = dict()
+    for ds in datasets.prefetch_related('oudata_set'):
         for oud in ds.oudata_set.all():
+            sources[oud.uid] = ds.id
             ous[oud.uid] = (oud.shortname, oud.name)
         for our in ds.ourelation_set.all():
+            our_sources[(our.relation, our.ou_id)] = ds.id
             id_relations[our.relation][our.ou1_id] = our.ou2_id
     outree = dict()
+    source_ids = set(oud_sources.values()).union(our_sources.values())
     for k, v in id_relations.items():
-        toplevel = set()
+        toplevel = set() # toplevel OUs
         rel_ous = dict()
+        # build a list of OUs with lists for children, add all OUs to toplevel
         for uid, (shortname, name) in ous.items():
             rel_ous[uid] = (uid, shortname, name, [])
             toplevel.add(uid)
+        # add missing parents
         for child_id, parent_id in v.items():
             if parent_id not in toplevel:
                 toplevel.add(parent_id)
                 rel_ous[parent_id] = (parent_id, None, None, [])
+        # remove OUs with parents from toplevel, build children lists
         for child_id, parent_id in v.items():
             toplevel.discard(child_id)
             rel_ous[parent_id][3].append(rel_ous[child_id])
         rel_outree = []
+        # add toplevel OUs to output
         for i in toplevel:
             rel_outree.append(rel_ous[i])
-        outree[k] = rel_outree
-    return outree
+        outree[k] = rel_outree # tree for relation type k created
+    return outree, source_ids
  
+def outrees_at(timestamp=None, source=None):
+    datasets = _datasets_at(timestamp, source)
+    return outrees_from_datasets(datasets)
 
-def batch_from_datasets(timestamp=None):
+def ldapactionbatch_at(timestamp=None):
+    datasets = _datasets_at(timestamp)
+    return ldapactionbatch_from_datasets(datasets)
+
+PRIMARY_OU = [
+    "",
+]
+
+GROUP_MAPS = [
+    "CN=Users,CN={clanica_Id}",
+]
+
+GROUP_REGEX_REPLACE = [
+    ("CN=Users,CN=2300", "CN=Users,CN=FRI"),
+]
+
+PROPERTY_REGEX_REPLACE = [
+            
+]
+
+def ldapactionbatch_from_datasets(datasets):
     actionbatch = LDAPActionBatch(description=timestamp.isoformat())
-    # TODO create actual data for a single user
-    userdata = userdata_from_datasets(timestamp)
+    userdata, user_sources = userdata_from_datasets(datasets)
+    outrees, ou_sources = outrees_from_datasets(datasets)
+
+    for user, user_props in userdata.items():
+        pass
     return actionbatch
 
+class LDAPActionBatch(models.Model):
+    description = models.CharField(max_length=512, blank=True, default='')
+    actions = models.ManyToManyField('LDAPAction')
+    def apply(self):
+        pass
+
 class LDAPAction(models.Model):
+    ACTION_CHOICES = [
+        ('user_upsert', 'Upsert user data'),
+        ('add', 'Add'),
+        ('delete', 'Delete')]
+    sources = models.ManyToManyField('DataSet')
+    action = models.CharField(max_length=16, choices=ACTION_CHOICES)
+    dn = models.TextField()
     data = models.JSONField()
 
 class LDAPApply(models.Model):
