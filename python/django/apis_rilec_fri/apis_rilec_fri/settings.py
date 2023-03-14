444b1c8ea53cffc64680f1ed44bd8030e5693419
Gašper Fele-Žorž <polz@fri.uni-lj.si>
2023-03-14 13:25:06 +0100

    fix settings

--- python/django/apis_rilec_fri/apis_rilec_fri/settings.py
+++ python/django/apis_rilec_fri/apis_rilec_fri/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
-X-API-KEY = "ToleJeSkrivnost"
+X_API_KEY = "ToleJeSkrivnost"
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
