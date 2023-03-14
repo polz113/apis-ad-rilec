f0fd98948d912181a2e0bb16b56ef21915d47b54
Gašper Fele-Žorž <polz@fri.uni-lj.si>
2023-03-14 10:48:44 +0100

    Fix default settings

--- python/django/apis_rilec_fri/apis_rilec_fri/settings.py
+++ python/django/apis_rilec_fri/apis_rilec_fri/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+X-API-KEY = "ToleJeSkrivnost"
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -38,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "rest_framework_api_key",
+#    "rest_framework_api_key",
 ]
 
 MIDDLEWARE = [
