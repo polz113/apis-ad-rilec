9e563b700bfb2ad9462b1751e0cbcd4c504d78cd
Polz <polz@polz.si>
2023-03-19 17:34:19 +0100

    ALLOWED_HOSTS odslej v .env

--- python/django/apis_rilec_fri/apis_rilec_fri/settings.py
+++ python/django/apis_rilec_fri/apis_rilec_fri/settings.py
@@ -44,10 +44,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env.str('SECRET_KEY')
-# SECURITY WARNING: don't run with debug turned on in production!
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 
 # Application definition
