596f064206a00aced56bef1a99796a2c4a543399
polz <polz@fri.uni-lj.si>
2023-03-20 15:32:48 +0100

    dodan default za ALLOWED_HOSTS

--- python/django/apis_rilec_fri/apis_rilec_fri/settings.py
+++ python/django/apis_rilec_fri/apis_rilec_fri/settings.py
@@ -46,7 +46,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env.str('SECRET_KEY')
 
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
 
 
 # Application definition
