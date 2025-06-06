fbe7dbe27dfbe4c1afe3f1391c0a89dbaba35271
polz <polz@fri.uni-lj.si>
2025-06-06 12:09:16 +0200

    Popravljen seznam dovoljenih skupin

--- python/django/apis_rilec_fri/apis_rilec/views.py
+++ python/django/apis_rilec_fri/apis_rilec/views.py
@@ -390,7 +390,9 @@ def ldapobjectbatch_diff(request, pk, pk2):
     for i in DEFAULT_IGNORE_FIELDS:
         allowed_values[i] = None
     # TODO handle allowed values for memberof based on autogroups
-    allowed_values['MEMBEROF'] = get_groups()
+    allowed_values['MEMBEROF'] = set([j 
+        for i in apis_rilec.models.get_groups()
+            for j in i['distinguishedName']])
     missing_objs = list()
     added_obj_dns = set(obj1_dicts["dn"].keys())
     changed_objs = []
