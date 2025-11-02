36938cc35f536567624ab3c81dfdd4a8a5b62cf9
polz <polz@polz.si>
2025-11-02 21:53:34 +0100

    Dodan prikaz podatkov za DataSet in MergedUserData v admin

--- python/django/apis_rilec_fri/apis_rilec/admin.py
+++ python/django/apis_rilec_fri/apis_rilec/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.db import models
 from django import forms
+from django.urls import reverse
+from django.utils.html import format_html, format_html_join
+from django.utils.safestring import mark_safe
 from django.utils.translation import gettext_lazy as _
+import json
 
 from .models import DataSource, DataSet,\
     OUData, OURelation, MergedUserData, UserData, UserDataField,\
@@ -13,12 +17,24 @@ from .models import DataSource, DataSet,\
 # Register your models here.
 class DataSetAdmin(admin.ModelAdmin):
     raw_id_fields = ('source',)
-    readonly_fields = ('source',)
+    readonly_fields = ('source', 'data_links', 'source_data')
+
+    @admin.display(description="UserData")
+    def data_links(self, instance):
+        return format_html_join(
+            mark_safe("<br>"),
+            """<a href="{}">{}: {}({})</a>""",
+            [(reverse("admin:apis_rilec_userdata_change", args=(d.id,)), d.id, d.uid, d.sub_id) for d in instance.userdata_set.all()],
+        )
+    
+    @admin.display(description="Source data")
+    def source_data(self, instance):
+        return format_html("<pre>{}</pre>", json.dumps(instance.source.parsed_json(), indent=4))
 
 class DataSourceAdmin(admin.ModelAdmin):
     actions = ('to_datasets',)
     # list_display = ['str_data']
-    readonly_fields = ('str_data',)
+    readonly_fields = ('str_data', 'dataset_links')
     list_filter = ["source"] 
     search_fields = ["timestamp"] 
     
@@ -26,6 +42,15 @@ class DataSourceAdmin(admin.ModelAdmin):
     def str_data(self, instance):
         return instance.data.decode('utf-8')
         # return instance.str_data()
+
+    @admin.display(description="DataSet")
+    def dataset_links(self, instance):
+        # return "HEH:" + str(instance.dataset_set.all())
+        return format_html_join(
+            mark_safe("<br>"),
+            """<a href="{}">{}</a>""",
+            [(reverse("admin:apis_rilec_dataset_change", args=(d.id,)), d.id) for d in instance.dataset_set.all()],
+        )
 
     @admin.action(description='Turn into datasets')
     def to_datasets(self, request, queryset):
@@ -52,7 +77,7 @@ class UserDataFieldInline(admin.TabularInline):
 class OldUserdataFilter(admin.SimpleListFilter):
     # Human-readable title which will be displayed in the
     # right admin sidebar just above the filter options.
-    title = _("Used in MergedUserData")
+    title = _("Used in MultiuserData")
 
     # Parameter for the filter that will be used in the URL query.
     parameter_name = "old_unused"
@@ -105,6 +130,29 @@ class OUDataAdmin(admin.ModelAdmin):
 
 class MergedUserDataAdmin(admin.ModelAdmin):
     raw_id_fields = ['data']
+    readonly_fields = ['data_links', 'with_extra']
+    search_fields = ['uid',]
+    
+    @admin.display(description="UserData")
+    def data_links(self, instance):
+        return format_html_join(
+            mark_safe("<br>"),
+            """<a href="{}">{}: {}({})</a>""",
+            [(reverse("admin:apis_rilec_userdata_change", args=(d.id,)), d.id, d.uid, d.sub_id) for d in instance.data.all()],
+        )
+
+    @admin.display(description="As dicts")
+    def with_extra(self, instance):
+        s = mark_safe("")
+        for fieldset in instance.with_extra():
+            s += mark_safe("<ul>")
+            for k in sorted(fieldset.keys()):
+                s += format_html("<li><span>{}:</span>", k)
+                l = [(i,) for i in fieldset.getlist(k, [])]
+                s += format_html_join(",", "{}", l)
+                s += mark_safe("</li>")
+            s += mark_safe("</ul>")
+        return s
     # readonly_fields = ['data']
     #actions = ['to_ldapactionbatch']
     #@admin.action(description="Convert to LDAPActionBatch")
